8c0ae00dd5901d2f6b50e68ab6d54234b76aa94b
david <cdavidlopez@hotmail.com>
2019-09-23 20:46:01 -0500

    falta api rest

--- BLUEPRINTS_PART1/src/main/resources/static/js/apimock.js
+++ BLUEPRINTS_PART1/src/main/resources/static/js/apimock.js
@@ -1,37 +1,55 @@
-apimock=(function(){
-    let mockdata=[];
+apimock = (function() {
+  let mockdata = [];
 
-    mockdata["author1"]=[
-        {"author":"author1",
-         "points":
-            [{"x":10,"y":15},
-             {"x":3,"y":0},
-             {"x":2,"y":4},
-             {"x":7,"y":8}],
-         "name":"blueprint1"},
-        {"author":"author1",
-            "points":
-                [{"x":1,"y":12},
-                    {"x":5,"y":3},
-                    {"x":7,"y":1},
-                    {"x":8,"y":11},
-                    {"x":10,"y":12}],
-            "name":"blueprint2"},
-        ];
-    mockdata["author2"]=[
-        {"author":"author2",
-         "points":
-             [{"x":1,"y":1},
-              {"x":2,"y":2},
-              {"x":3,"y":3},
-             ],
-         "name":"blueprintb"}];
+  mockdata["author1"] = [
+    {
+      author: "author1",
+      points: [
+        { x: 100, y: 150 },
+        { x: 300, y: 0 },
+        { x: 200, y: 40 },
+        { x: 70, y: 80 }
+      ],
+      name: "blueprint1"
+    },
+    {
+      author: "author1",
+      points: [
+        { x: 100, y: 120 },
+        { x: 50, y: 300 },
+        { x: 70, y: 100 },
+        { x: 80, y: 110 },
+        { x: 100, y: 120 }
+      ],
+      name: "blueprint2"
+    }
+  ];
+  mockdata["author2"] = [
+    {
+      author: "author2",
+      points: [
+        { x: 200, y: 200 },
+        { x: 400, y: 200 },
+        { x: 300, y: 100 },
+        { x: 200, y: 200 },
+        { x: 200, y: 400 },
+        { x: 400, y: 400 },
+        { x: 400, y: 200 }
+      ],
+      name: "casa"
+    }
+  ];
 
-    return {
-        getBlueprintsByAuthor:function(name,callback){
-            return callback(
-                mockdata[name]
-            );
-        }
-    };
+  return {
+    getBlueprintsByAuthor: function(name, callback) {
+      return callback(mockdata[name]);
+    },
+    getBlueprintsByNameAndAuthor: function(author, name, callback) {
+      return callback(
+        mockdata[author].filter(obj => {
+          return obj.name === name;
+        })
+      );
+    }
+  };
 })();
--- BLUEPRINTS_PART1/src/main/resources/static/js/app.js
+++ BLUEPRINTS_PART1/src/main/resources/static/js/app.js
@@ -1,45 +1,76 @@
-var BlueprintModule = (function () {
+var BlueprintModule = (function() {
+  var _author;
+  var _authorBlueprint = [];
 
-    var _author;
-    var _authorBlueprint = [];
+  var _mapNamePoints = function(blueprints) {
+    return blueprints.map(function(blueprint) {
+      return { name: blueprint.name, points: blueprint.points.length };
+    });
+  };
 
-    var _mapNamePoints = function (blueprints) {
-        return blueprints.map(function (blueprint) {
-            return {name:blueprint.name, points:blueprint.points.length};
-        });
-    };
+  var _sumPoints = function(blueprints) {
+    var sum = blueprints.reduce(function(total, currentValue) {
+      return total + currentValue.points;
+    }, 0);
+    $("#blueprintSum > h3").text("Total user points: " + sum);
+  };
 
-    var _sumPoints= function(blueprints){
-        var sum = blueprints.reduce(function (total, currentValue) {
-            return total + currentValue.points;
-        }, 0);
-        $("#blueprintSum > h3").text("Total user points: " + sum);
-    }
+  //blueprints : mapeo de blueprints
+  var _genTable = function(blueprints) {
+    _sumPoints(blueprints);
+    $("#blueprintTable > tbody").empty(); //Limpia el cuerpo de la tabla para otros datos
+    blueprints.map(function(blueprint) {
+      $("#blueprintTable > tbody").append(
+        "<tr> <td>" +
+          blueprint.name +
+          "</td>" +
+          "<td>" +
+          blueprint.points +
+          "</td>" +
+          "<td><form><button type='button' class='btn btn-primary' onclick='BlueprintModule.openPlane( \"" +
+          _author +
+          '" , "' +
+          blueprint.name +
+          "\")' >Open</button></form></td>" +
+          "</tr>"
+      );
+    });
+  };
 
-    //blueprints : mapeo de blueprints
-    var _genTable = function (blueprints) {
-        _sumPoints(blueprints);
-        $("#blueprintTable > tbody").empty(); //Limpia el cuerpo de la tabla para otros datos
-        blueprints.map(function (blueprint) {
-            $("#blueprintTable > tbody").append(
-                "<tr> <td>" + blueprint.name + "</td>" + "<td>"+ blueprint.points +"</td></tr>"
-            )
-        });
-    };
+  var _pintar = function(blueprint) {
+    $("#tituloPlano").text("Current blueprint: " + blueprint[0].name);
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.beginPath();
+    var anterior;
+    blueprint[0].points.map(function(point) {
+      if (!anterior) {
+        anterior = point;
+        ctx.moveTo(anterior.x, anterior.y);
+      } else {
+        ctx.lineTo(point.x, point.y);
+        ctx.stroke();
+      }
+    });
+  };
+  var changeAuthorName = function(author) {
+    _author = author;
+  };
 
-    var changeAuthorName = function (author) {
-        _author = author;
-    };
+  var updateListPlans = function(author) {
+    changeAuthorName(author);
+    $("#blueprintAuthorName > h2").text(author + "'s blueprints: ");
+    _genTable(apimock.getBlueprintsByAuthor(author, _mapNamePoints));
+  };
 
-    var updateListPlans = function (author) {
-        changeAuthorName(author);
-        $("#blueprintAuthorName > h2").text(author + "'s blueprints: ");
-        _genTable(apimock.getBlueprintsByAuthor(author,_mapNamePoints));
-    };
+  var openPlane = function(author, name) {
+    apimock.getBlueprintsByNameAndAuthor(author, name, _pintar);
+  };
 
-    return {
-        changeAuthorName : changeAuthorName,
-        updateListPlans : updateListPlans
-    };
-
+  return {
+    changeAuthorName: changeAuthorName,
+    updateListPlans: updateListPlans,
+    openPlane: openPlane
+  };
 })();
